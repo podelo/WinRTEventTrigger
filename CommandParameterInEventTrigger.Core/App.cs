4339eb35537370ce5f74756c93c8445cb3f2acba
podelo <podelo34@gmail.com>
2013-06-06 11:53:54 +0200

    Commit

---
+++ CommandParameterInEventTrigger.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore.IoC;
+
+namespace CommandParameterInEventTrigger.Core
+{
+    public class App: Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+				
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
---
+++ CommandParameterInEventTrigger.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace CommandParameterInEventTrigger.Core.ViewModels
+{
+    public class FirstViewModel : MvxViewModel
+    {
+        public string _First = string.Empty;
+        public string First
+        {
+            get
+            {
+                return _First;
+            }
+
+            set
+            {
+                if (_First == value)
+                    return;
+
+                _First = value;
+                RaisePropertyChanged(() => First);
+            }
+        }
+
+        public string _Second = string.Empty;
+        public string Second
+        {
+            get
+            {
+                return _Second;
+            }
+
+            set
+            {
+                if (_Second == value)
+                    return;
+
+                _Second = value;
+                RaisePropertyChanged(() => Second);
+            }
+        }
+
+
+        public ICommand UpdateText
+        {
+            get
+            {
+                return new MvxCommand<object>((textSearch) =>
+                    {
+                        Second = textSearch.ToString();
+                    });
+            }
+        }
+
+    }
+}
